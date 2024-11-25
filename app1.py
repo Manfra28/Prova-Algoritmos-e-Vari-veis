c10e0772f89abec3125b2fdeec3513b19e12732a
Manfra28 <omanfra28@gmail.com>
2024-11-25 10:24:11 -0300

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,9 @@
+# Solicitar ao usuário os valores da base e da altura
+base = float(input("Digite o valor da base do retângulo: "))
+altura = float(input("Digite o valor da altura do retângulo: "))
+
+# Calcular a área do retângulo
+area = base * altura
+
+# Exibir o resultado
+print(f"A área do retângulo é: {area}")
